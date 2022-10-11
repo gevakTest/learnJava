6956192ade0ac158d9673b5ac77bdd9385b7d494
oleh.hevak <asd@asd.com>
2022-10-12 00:05:54 +0300

    Add first task answer

--- src/main/java/basic/Task1.java
+++ src/main/java/basic/Task1.java
@@ -3,7 +3,22 @@ package basic;
 public class Task1 {
 
     public int countVowels(String text) {
-        throw new UnsupportedOperationException("This method isn't supported yet");
+        char[] characters = text.toCharArray();
+        int i = 0;
+        for(char character : characters) {
+            if (isVowel(character)){
+                i++;
+            }
+        }
+        return i;
+    }
+
+    private static boolean isVowel(char character) {
+        return character == 'a'
+                || character == 'e'
+                || character == 'i'
+                || character == 'o'
+                || character == 'u';
     }
 
 }
